93c3a90f7b10fbd71462baeba505a378a836b200
crameth <crameth@users.noreply.github.com>
2022-10-03 22:57:45 +0800

    added 7 day leaderboard

--- src/bot.py
+++ src/bot.py
@@ -116,5 +116,33 @@ async def on_message(message):
         
         await message.channel.send(f'{user_scores_str}')
         return
+    elif message.channel.id == MONITOR_CHANNEL_ID and message.content == ('!wordle-7dlb'):
+        channel = client.get_channel(LEADERBOARD_CHANNEL_ID)
+        records = [m async for m in channel.history(limit=7)]
+
+        # calculate total scores for each person
+        user_scores = {}
+        for r in records:
+            if r.content.startswith(f'**'):
+                # check if user score is already submitted for that particular series
+                scores = r.content.split('\n')[1:]
+
+                for s in scores:
+                    user_id = s.split(' ')[0]
+                    score = int(s.split(' ')[1][2:3])
+                    if user_id in user_scores.keys():
+                        user_scores[user_id]["score"] = user_scores[user_id]["score"] + score
+                        user_scores[user_id]["count"] = user_scores[user_id]["count"] + 1
+                    else:
+                        user_scores[user_id] = {"score": score, "count": 1}
+
+        user_scores = {k: v for k, v in sorted(user_scores.items(), key=lambda item: item[1]["score"], reverse=True)}
+        
+        user_scores_str = "**7-day Leaderboard**\n"
+        for i, (k, v) in enumerate(user_scores.items()):
+            user_scores_str = user_scores_str + f'{i+1}. {k} `{v["score"]}` ({round(v["score"]/v["count"],2)})\n'
+        
+        await message.channel.send(f'{user_scores_str}')
+        return
 
 client.run(TOKEN)
